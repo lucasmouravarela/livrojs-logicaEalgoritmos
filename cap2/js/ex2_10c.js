dade9545f46a3ff6aeab58d65267805f194d6d25
Lucas Varela <lucasmouravarela@gmail.com>
2021-08-27 14:20:04 -0300

    Modificando o uso das variáveis

--- cap2/js/ex2_10c.js
+++ cap2/js/ex2_10c.js
@@ -1,17 +1,21 @@
+const inputProduto = document.getElementById("inProduto");
+const inputPreco = document.getElementById("inPreco");
+const outputPromo = document.getElementById("outPromocao");
+const outTerceiraUnid = document.getElementById("outTerceiraUnidade");
+
 function verPromocao() {
-    var inputProduto = document.getElementById('inProduto');
-    var inputPreco = document.getElementById('inPreco');
-    var outputPromo = document.getElementById('outPromocao');
-    var outTerceiraUnid = document.getElementById('outTerceiraUnidade');
+  let produto = inputProduto.value;
+  let preco = Number(inputPreco.value);
 
-    var produto = inputProduto.value;
-    var preco = Number(inputPreco.value);
+  let promoUnidade = preco / 2;
+  let promo = preco * 2 + promoUnidade;
 
-    var promoUnidade = preco / 2;
-    var promo =  (preco * 2) + promoUnidade;
-
-    outputPromo.textContent = `${produto} - Promoção: Leve 3 por R$: ${promo.toFixed(2)}`;
-    outTerceiraUnid.textContent = `O 3º produto custa apenas R$: ${promoUnidade.toFixed(2)}`;
+  outputPromo.textContent = `${produto} - Promoção: Leve 3 por R$: ${promo.toFixed(
+    2
+  )}`;
+  outTerceiraUnid.textContent = `O 3º produto custa apenas R$: ${promoUnidade.toFixed(
+    2
+  )}`;
 }
-var btPromo = document.getElementById('btPromocao');
-btPromo.addEventListener('click', verPromocao);
+var btPromo = document.getElementById("btPromocao");
+btPromo.addEventListener("click", verPromocao);
